6a0698cfc322489feb0b0ab1a6cb24c9bad95f38
simpleem <865645941@qq.com>
2018-03-25 00:31:11 +0800

    init project

---
+++ JSTestGit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSTestGit
+//
+//  Created by simpleem on 2018/3/25.
+//  Copyright © 2018年 simpleem. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSTestGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSTestGit
+//
+//  Created by simpleem on 2018/3/25.
+//  Copyright © 2018年 simpleem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
